0848bf3363fbf9067130408f241e71a02c9892e1
KFarrow11 <117604206+KFarrow11@users.noreply.github.com>
2025-01-23 10:55:03 +0000

    colour and appearance
    changed colour blocks,
    amended questions
    reminder - replace image

---
+++ colours.R
@@ -0,0 +1,19 @@
+1. **Yellow Team**: `#FFD700`
+2. **Blue Team**: `#1E90FF`
+3. **Green Team**: `#4EBA8B`
+4. **Red Team**: `#DE0013`
+5. **Purple Team**: `#800080`
+6. **Orange Team**: `#FFA500`
+7. **Sky Blue**: `#56B4E9`
+8. **Orange**: `#D55E00`
+9. **Purple**: `#9B59B6`
+10. **Teal**: `#1F77B4`
+11. **Pink**: `#F39C12`
+12. **Gray**: `#BDBDBD`
+13. **Bright Red**: `#E41A1C`
+14. **Bright Green**: `#4CAF50`
+15. **Bright Orange**: `#FF7F00`
+16. **Bright Purple**: `#9B30FF`
+17. **Bright Blue**: `#1E90FF`
+18. **Bright Grey**: `#B0B0B0`
+
